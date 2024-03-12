2169f4d68237327fa8a68518ec22a65ff5c7ab69
Davide-Morellato <morellato.davide@yahoo.it>
2024-03-12 17:42:12 +0100

    ADD: verify control

--- Pari e Dispari/app.js
+++ Pari e Dispari/app.js
@@ -24,67 +24,92 @@ console.log('numero utente: ', userNumber)
 
 
 
-// DICHIARO UNA FUNZIONE randomPc
-// IN CUI CREO UNA VARIABILE randomNum PER GENERARE UN NUMERO CASUALE PER IL PC (da 1 a 5)
+// VERIFICA DI CONTROLLO prompt:
+// SE IL NUMERO INSERITO DALL'UTENTE (userNumber) E' > 5  AND (&&)
+// IL NUMERO INSERITO DALL'UTENTE (userNumber) E' <= 0  AND  (&&)
+// LA SCELTA TRA PARI E DISPARI DALL'UTENTE (evenOdd) E' DIVERSA DA 'pari'  OR  (||)
+// LA SCELTA TRA PARI E DISPARI DALL'UTENTE (evenOdd) E' DIVERSA DA 'dispari'
 
-function randomPc(){
+    // ALLORA PRESENTA UN ALERT ALL'UTENTE
+    // STAMPA IN CONSOLE 'valori non validi'
 
-    const randomNum = parseInt(Math.floor(Math.random() * 5) + 1)
+// ALTRIMENTI 
+    // SVOLGI LE FUNZIONI
 
-    return randomNum
+if(userNumber > 5 &&
+    userNumber <= 0 &&
+    evenOdd !== 'pari' ||
+    evenOdd !== 'dispari'){
 
-}
+    alert('digitare correttamente la tua scelta')
 
-// CREO UNA VARIABILE PER INVOCARE LA FUNZIONE randomPc
-
-const random = randomPc()
-console.log('numero casuale: ', random)
-
-
-
-// CREO UNA VARIABILE PER SOMMARE I DUE NUMERI (userNumber + random)
-
-const sum = userNumber + random
-
-console.log('somma: ', sum)
-
-
-
-// DICHIARO UNA FUNZIONE isEvenOdd
-// IN CUI CREO UNA VARIABILE result PER VERIFICARE SE LA SOMMA DEI NUMERI sum
-// SIA PARI O DISPARI (sfrutto il modulo % 2)
-
-function isEvenOdd(){
-
-    const result = sum % 2
-
-    return result
-
-}
-
-// CREO UNA VARIABILE PER INVOCARE LA FUNZIONE isEvenOdd()
-
-const rest = isEvenOdd()
-
-
-// SE IL NUMERO OTTENUTO HA RESTO STRETTAMENTE UGUALE (===) 0 (pari) AND  LA SCELTA DELL'UTENTE E' PARI
-// ALLORA STAMPA HAI VINTO
-
-// ALTRIMENTI SE IL NUMERO OTTENUTO HA RESTO DIVERSO (!==) DA 0  AND  LA SCELTA DELL'UTENTE E' DISPARI
-// ALLORA STAMPA HAI VINTO
-
-// ALTRIMENTI (se nessuna delle due)
-// ALLORA STAMPA HAI PERSO
-
-if(rest === 0 && evenOdd === 'pari'){
-
-    console.log('è pari: hai vinto!')
-
-}else if (rest !== 0 && evenOdd === 'dispari'){
-
-    console.log('è dispari: hai vinto!')
+    console.log('VALORI NON VALIDI')
 
 } else {
+    
+    // DICHIARO UNA FUNZIONE randomPc
+    // IN CUI CREO UNA VARIABILE randomNum PER GENERARE UN NUMERO CASUALE PER IL PC (da 1 a 5)
+    
+    function randomPc(){
+    
+        const randomNum = parseInt(Math.floor(Math.random() * 5) + 1)
+    
+        return randomNum
+    
+    }
+    
+    // CREO UNA VARIABILE PER INVOCARE LA FUNZIONE randomPc
+    
+    const random = randomPc()
+    console.log('numero casuale: ', random)
+    
+    
+    
+    // CREO UNA VARIABILE PER SOMMARE I DUE NUMERI (userNumber + random)
+    
+    const sum = userNumber + random
+    
+    console.log('somma: ', sum)
+    
+    
+    
+    // DICHIARO UNA FUNZIONE isEvenOdd
+    // IN CUI CREO UNA VARIABILE result PER VERIFICARE SE LA SOMMA DEI NUMERI sum
+    // SIA PARI O DISPARI (sfrutto il modulo % 2)
+    
+    function isEvenOdd(){
+    
+        const result = sum % 2
+    
+        return result
+    
+    }
+    
+    // CREO UNA VARIABILE PER INVOCARE LA FUNZIONE isEvenOdd()
+    
+    const rest = isEvenOdd()
+    
+    
+    // SE IL NUMERO OTTENUTO HA RESTO STRETTAMENTE UGUALE (===) 0 (pari) AND  LA SCELTA DELL'UTENTE E' PARI
+    // ALLORA STAMPA HAI VINTO
+    
+    // ALTRIMENTI SE IL NUMERO OTTENUTO HA RESTO DIVERSO (!==) DA 0  AND  LA SCELTA DELL'UTENTE E' DISPARI
+    // ALLORA STAMPA HAI VINTO
+    
+    // ALTRIMENTI (se nessuna delle due)
+    // ALLORA STAMPA HAI PERSO
+    
+    if(rest === 0 && evenOdd === 'pari'){
+    
+        console.log('è pari: hai vinto!')
+    
+    }else if (rest !== 0 && evenOdd === 'dispari'){
+    
+        console.log('è dispari: hai vinto!')
+    
+    } else {
+    
+        console.log('hai perso!')
+    }
 
-    console.log('hai perso!')
 }
